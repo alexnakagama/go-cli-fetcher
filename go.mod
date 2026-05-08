a434519b9e49cb828a0530c66c35f11633aefa59
alex <alexnakagama4@gmail.com>
2026-05-08 15:54:45 -0300

    added structure for the testing and func to create a new client that gets the token

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/alexnakagama/go-cli-fetcher
 
 go 1.25.0
 
-require github.com/spf13/cobra v1.10.2
+require (
+	github.com/google/go-github/v86 v86.0.0
+	github.com/spf13/cobra v1.10.2
+	golang.org/x/oauth2 v0.36.0
+)
 
 require (
+	github.com/google/go-querystring v1.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 )
--- internal/github/client.go
+++ internal/github/client.go
@@ -1,1 +1,21 @@
 package github
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/v86/github"
+	"golang.org/x/oauth2"
+)
+
+func NewClient() (*github.Client, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return nil, fmt.Errorf("GITHUB_TOKEN no está configurado")
+	}
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(context.Background(), ts)
+	return github.NewClient(tc), nil
+}
