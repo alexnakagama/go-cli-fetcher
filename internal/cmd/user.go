667b3d0f0d4a5e391c417492728bb721008c68c5
alex <alexnakagama4@gmail.com>
2026-05-12 11:43:51 -0300

    added the search url command

--- internal/cmd/user.go
+++ internal/cmd/user.go
@@ -15,9 +15,20 @@ var userGetInfoCmd = &cobra.Command{
 	Use:   "detail [username]",
 	Short: "Fetch information of a github user",
 	Long:  `Fetch detailed information about a GitHub user, including its name, description, followers/following, and more.`,
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		username := args[0]
 		return github.SearchUserInfo(username)
+	},
+}
+
+var userGetUrlCmd = &cobra.Command{
+	Use:   "url [username]",
+	Short: "Fetch url from a github user",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		username := args[0]
+		return github.SearchUserUrl(username)
 	},
 }
 
